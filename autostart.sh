25c7adb23e97c7772192de023093b75f09111d5d
solaryan <466010106@qq.com>
2024-02-16 19:00:00 +0800

    init

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 xrandr --output HDMI-A-0 --mode 2560x1440 --rate 144 
 xrandr --output eDP --off
-feh --bg-fill --no-fehbg ~/Pictures/星穹铁道/97911234_p0.jpg
+# feh --bg-fill --no-fehbg ./res/xingandying.jpg
+`~/Scripts/rand_bg.sh &`
