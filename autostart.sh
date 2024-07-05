1d4f1b6d41b3401208414058e2218a0dd048fb4f
solaryan <466010106@qq.com>
2024-07-05 21:45:32 +0800

    --

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-xrandr --output HDMI-A-0 --mode 2560x1440 --rate 144 
-xrandr --output eDP --off
+if xrandr | grep "HDMI-A-0 connect"; then
+	xrandr --output HDMI-A-0 --mode 2560x1440 --rate 144
+	xrandr --output eDP --off
+else
+	xrandr --output eDP --mode 1920x1200 --rate 120
+fi
+#xrandr --output HDMI-A-0 --mode 2560x1440 --rate 144 
+#xrandr --output eDP --off
+#xrandr --output eDP --mode 1920x1200 --rate 120
 # feh --bg-fill --no-fehbg ./res/xingandying.jpg
 `~/Scripts/rand_bg.sh &`
